536ab5eda27437e5e5209616bae836a5e71a422e
dma <dorian.magni@globaz.ch>
2019-03-02 16:22:41 +0100

    Ajout de lib et config pour slf4j

--- src/main/java/ch/hesso/webminig/App.java
+++ src/main/java/ch/hesso/webminig/App.java
@@ -8,18 +8,9 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        final int MAX_CRAWL_DEPTH = 1;
         final int NUMBER_OF_CRAWELRS = 2;
-        final String CRAWL_STORAGE = "/data/crawl/root";
 
-        /*
-         * Instantiate crawl config
-         */
-        CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(CRAWL_STORAGE);
-        config.setMaxDepthOfCrawling(MAX_CRAWL_DEPTH);
-        config.setMaxPagesToFetch(10);
+        CrawlConfig config = initialiseConfig();
 
         /*
          * Instantiate controller for this crawl.
@@ -29,16 +20,31 @@ public class App {
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
         CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-
         /*
          * Add seed URLs
          */
-        controller.addSeed("https://www.lesnumeriques.com/");
+        controller.addSeed("http://www.commitstrip.com/fr/");
 
         /*
          * Start the crawl.
          */
         controller.start(Crawler.class, NUMBER_OF_CRAWELRS);
+    }
+
+    private static CrawlConfig initialiseConfig() {
+        CrawlConfig config = new CrawlConfig();
+        config.setMaxConnectionsPerHost(10);
+        config.setSocketTimeout(5000);
+        config.setCrawlStorageFolder("tmp");
+        config.setIncludeHttpsPages(true);
+        //minimum 250ms for tests
+        config.setPolitenessDelay(250);
+        config.setUserAgentString("crawler4j/WEM/2019/");//Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0
+        //max 2 -3 levels for tests on large website
+        config.setMaxDepthOfCrawling(2);
+        // -1 for unlimited number of pages
+        config.setMaxPagesToFetch(2000);
+        return config;
     }
 
 }
--- src/main/java/ch/hesso/webminig/Crawler.java
+++ src/main/java/ch/hesso/webminig/Crawler.java
@@ -2,20 +2,47 @@ package ch.hesso.webminig;
 
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.jsoup.Jsoup;
 
 import java.util.regex.Pattern;
 
 public class Crawler extends WebCrawler {
-    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|png|mp3|mp4|zip|gz))$");
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg|png|mp3|mp4|zip|gz|txt|pdf|))$");
+
+    private final HttpSolrClient solrClient =
+            new HttpSolrClient.Builder("http://localhost:8983/solr/")
+                    .withConnectionTimeout(10000)
+                    .withSocketTimeout(60000)
+                    .build();
 
     @Override
-    public boolean shouldVisit(Page referringPage, WebURL url) {
-        return !FILTERS.matcher(url.getURL().toLowerCase()).matches();
+    public boolean shouldVisit(Page page, WebURL url) {
+        return !FILTERS.matcher(url.getURL().toLowerCase()).matches()
+                && page.getStatusCode() >= 200 && page.getStatusCode() < 300;
     }
 
     @Override
     public void visit(Page page) {
-        System.out.println(page.getWebURL());
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String title = Jsoup.parse(htmlParseData.getHtml()).select(".entry-title").text();
+            if (!title.isEmpty()) {
+                System.out.println(title+" "+page.getWebURL().getURL());
+
+/*                final SolrInputDocument doc = new SolrInputDocument();
+                doc.addField("id", UUID.randomUUID().toString());
+                doc.addField("title", title);
+
+                try {
+                    final UpdateResponse updateResponse = solrClient.add("techproducts", doc);
+                    solrClient.commit("techproducts");
+                } catch (SolrServerException | IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        }
     }
 }
