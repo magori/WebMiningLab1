1f2a98fd17c4a4655a251f48beffa80abfa62d17
dma <dorian.magni@globaz.ch>
2019-03-17 00:11:08 +0100

    Rennomage

--- src/main/java/ch/hesso/webminig/Search.java
+++ src/main/java/ch/hesso/webminig/Search.java
@@ -15,33 +15,26 @@ import java.io.IOException;
 public class Search {
     private SolrClient solrClient = SolrUtil.connectToSolrClient("wemlabo1");
 
-    // Args represent the query
-    public static void main(String... args) {
-        String query = args.length == 0 ? "*" : String.join(" ", args);
+    public static void main(String... args) throws IOException, SolrServerException {
+        String searchText = args.length == 0 ? "*" : String.join(" ", args);
         Search search = new Search();
-        search.executeQuery(query);
+        search.executeQuery(searchText);
     }
 
-    private void executeQuery(final String query) {
-        try {
-           /* final QueryResponse response = solr.query(queryParams);
-            final SolrDocumentList documents = response.getResults();*/
-            SolrQuery q = new SolrQuery(String.format("(%s:%s)^3 (%s:'%s')^2 (%s:%s)^1", Fields.TITLE, query,Fields.CONTENT,query,Fields.TAGS,
-                                                      query));
-            q.setFields(Fields.TITLE, Fields.CONTENT, Fields.TAGS, Fields.UPVOTES, Fields.ANSWERED, Fields.URL, Fields.DATE, Fields.ID);
-            q.setRows(20);
-            final QueryResponse queryResponse = solrClient.query(q);
-            final SolrDocumentList documents = queryResponse.getResults();
+    private void executeQuery(final String searchText) throws IOException, SolrServerException {
+        SolrQuery q = new SolrQuery(String.format("(%s:%s)^3 (%s:'%s')^2 (%s:%s)^1", Fields.TITLE, searchText, Fields.CONTENT, searchText, Fields.TAGS,
+                                                  searchText));
+        q.setFields(Fields.TITLE, Fields.CONTENT, Fields.TAGS, Fields.UPVOTES, Fields.ANSWERED, Fields.URL, Fields.DATE, Fields.ID);
+        q.setRows(20);
+        final QueryResponse queryResponse = solrClient.query(q);
+        final SolrDocumentList documents = queryResponse.getResults();
 
-            System.out.println("\n===========================================");
-            System.out.println("QUERY: '" + query + "'");
-            System.out.println("NUMBER OF DOCUMENTS FOUND: " + documents.getNumFound());
-            System.out.println("TOTAL DOCUMENTS IN INDEX: " + this.countNbDocumentsInIndex());
-            System.out.println("===========================================");
-            documents.forEach(Search::printFields);
-        } catch (SolrServerException | IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("\n------------------------------------------");
+        System.out.println("SEARCH: '" + searchText + "'");
+        System.out.println("NUMBER OF DOCUMENTS FOUND: " + documents.getNumFound());
+        System.out.println("TOTAL DOCUMENTS " + this.countNbDocumentsInIndex());
+        System.out.println("---------------------------------------------\n");
+        documents.forEach(Search::printFields);
     }
 
     private long countNbDocumentsInIndex() throws IOException, SolrServerException {
@@ -53,7 +46,7 @@ public class Search {
 
     private static void printFields(SolrDocument document) {
         document.getFieldNames().forEach(s -> System.out.println(s + ": " + document.get(s)));
-        System.out.println("===========================================");
+        System.out.println("________________________________________________________________________");
     }
 
 }
--- src/test/java/ch/hesso/webminig/SearchTest.java
+++ src/test/java/ch/hesso/webminig/SearchTest.java
@@ -1,15 +1,18 @@
 package ch.hesso.webminig;
 
+import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 class SearchTest {
     @Test
-    void testMain_notEmpt() {
+    void testMain_notEmpt() throws IOException, SolrServerException {
         Search.main("my textfield in my JFrame java");
     }
 
     @Test
-    void testMain_empty() {
+    void testMain_empty() throws IOException, SolrServerException {
         Search.main();
     }
 }
